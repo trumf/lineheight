ad3ea5f3928a4a3fcadb749defa6fea66e7095e5
Simon Rosenqvist <blundrs@gmail.com>
2024-01-21 18:00:56 +0700

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const extraShortTexts = ["extra short text"];
+
 const shortTexts = [
   "Everyone has the right to life, liberty and security of person.",
-  "No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited in all their forms.",
+
   "No one shall be subjected to torture or to cruel, inhuman or degrading treatment or punishment.",
   "Everyone has the right to recognition everywhere as a person before the law.",
   "No one shall be subjected to arbitrary arrest, detention or exile.",
@@ -11,22 +13,24 @@ const shortTexts = [
   "Everyone has the right to seek and to enjoy in other countries asylum from persecution.",
   "Everyone, without any discrimination, has the right to equal pay for equal work.",
   "Everyone has the right to form and to join trade unions for the protection of his interests.",
-  "Everyone has the right to rest and leisure, including reasonable limitation of working hours and periodic holidays with pay.",
-  "Everyone has duties to the community in which alone the free and full development of his personality is possible.",
+
   "",
 ];
 const mediumTexts = [
   "All human beings are born free and equal in dignity and rights. They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood.",
-  "Everyone has the right to freedom of opinion and expression; this right includes freedom to hold opinions without interference and to seek, receive and impart information and ideas through any media and regardless of frontiers.",
-  "The will of the people shall be the basis of the authority of government; this will shall be expressed in periodic and genuine elections which shall be by universal and equal suffrage and shall be held by secret vote or by equivalent free voting procedures.",
+  "Everyone has the right to rest and leisure, including reasonable limitation of working hours and periodic holidays with pay.",
+  "No one shall be held in slavery or servitude; slavery and the slave trade shall be prohibited in all their forms.",
   "Everyone has the right to work, to free choice of employment, to just and favourable conditions of work and to protection against unemployment.",
-  "Education shall be directed to the full development of the human personality and to the strengthening of respect for human rights and fundamental freedoms. It shall promote understanding, tolerance and friendship among all nations, racial or religious groups, and shall further the activities of the United Nations for the maintenance of peace.",
+  "Everyone has duties to the community in which alone the free and full development of his personality is possible.",
   "Everyone has the right to the protection of the moral and material interests resulting from any scientific, literary or artistic production of which he is the author.",
   "",
 ];
 const longTexts = [
   "Everyone is entitled to all the rights and freedoms set forth in this Declaration, without distinction of any kind, such as race, colour, sex, language, religion, political or other opinion, national or social origin, property, birth or other status. Furthermore, no distinction shall be made on the basis of the political, jurisdictional or international status of the country or territory to which a person belongs, whether it be independent, trust, non-self-governing or under any other limitation of sovereignty.",
   "",
+  "Everyone has the right to freedom of opinion and expression; this right includes freedom to hold opinions without interference and to seek, receive and impart information and ideas through any media and regardless of frontiers.",
+  "The will of the people shall be the basis of the authority of government; this will shall be expressed in periodic and genuine elections which shall be by universal and equal suffrage and shall be held by secret vote or by equivalent free voting procedures.",
+  "Education shall be directed to the full development of the human personality and to the strengthening of respect for human rights and fundamental freedoms. It shall promote understanding, tolerance and friendship among all nations, racial or religious groups, and shall further the activities of the United Nations for the maintenance of peace.",
   "Be exactly who you want to be, do what you want to do I am he and she is she but you're the only you No one else has got your eyes, can see the things you see Its up to you to change your life and my lifes up to me The problems that you suffer from are problems that you make The shit we have to climb through is the shit we choose to take If you dont like the life you live, change it now it's yours Nothing has effects if you don't recognize the cause",
   "",
 ];
@@ -41,11 +45,16 @@ const fontData = [
   ["Bodoni Moda, serif", [400, 500, 900]],
   ["Chivo Mono, monospace", [100, 500, 900]],
   ["Dancing Script, cursive", [500]],
+  ["Dosis, sans-serif", [200, 500, 800]],
   ["Hepta Slab, serif", [100, 500, 900]],
   ["IBM Plex Mono, monospace", [100, 500, 700]],
   ["Imbue, sans-serif", [100, 500, 900]],
-  ["Josefin Sans, sans-serif", [100, 500, 700]],
-  ["Shantell Sans, sans-serif", [300, 500, 900]],
+  ["Inter Tight, sans-serif", [100, 500, 900]],
+  ["Josefin Sans, sans-serif", [100, 400, 700]],
+  ["Josefin Slab, serif", [100, 400, 700]],
+  ["Montserrat, sans-serif", [100, 400, 900]],
+  ["Oswald, sans-serif", [200, 400, 700]],
+  ["Shantell Sans, sans-serif", [300, 500, 800]],
 ];
 
 const sliderValues = [
@@ -75,9 +84,7 @@ const getRandomSliderValues = () => {
 export default function App() {
   const [activeDiv, setActiveDiv] = useState(null);
   const [randomSize, setRandomSize] = useState(0);
-  const [exampleText, setExampleText] = useState(
-    "Lorem ipsum dolor sit amet consectetur"
-  );
+  const [exampleText, setExampleText] = useState("");
 
   useEffect(() => {
     // Function to generate a random font size between 8 and 100
@@ -90,12 +97,14 @@ export default function App() {
   }, []); // Empty dependency array ensures the effect runs only once on mount
 
   useEffect(() => {
-    if (randomSize < 20) {
+    if (randomSize < 18) {
       setExampleText(getRandomListItem(longTexts));
-    } else if (randomSize >= 20 && randomSize < 48) {
+    } else if (randomSize >= 18 && randomSize < 40) {
       setExampleText(getRandomListItem(mediumTexts));
-    } else {
+    } else if (randomSize >= 40 && randomSize < 72) {
       setExampleText(getRandomListItem(shortTexts));
+    } else {
+      setExampleText(getRandomListItem(extraShortTexts));
     }
   }, [randomSize]);
 
